47d0b06f19de1cea7bb487548f9b83a47c5b5099
Xelilov <khalidkhkh@code.edu.az>
2018-03-10 22:47:32 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+// <<<<<<<<<<<<<<<<<<<<<<< Index.html<<<<<<<<<<<<<<<<<<<<<< \\
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoHeight:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+
+        }
+    }
+})
